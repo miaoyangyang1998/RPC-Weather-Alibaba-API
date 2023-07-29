20221e9dbf3a6c6a9325dac0c8d7c4e58fb311a5
miaoyangyang <miaoyangyang1998@foxmail.com>
2023-07-29 23:37:15 +0800

    首次提交，创建仓库

---
+++ src/main/java/com/ustc/weather/RpcWeatherAlibabaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ustc.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcWeatherAlibabaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcWeatherAlibabaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ustc/weather/RpcWeatherAlibabaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ustc.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcWeatherAlibabaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
